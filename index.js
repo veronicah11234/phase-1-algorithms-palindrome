c7e122f331e557d6d4a49a5bef9c5df466d63eaf
miriam <miriamwangui525@gmail.com>
2024-01-23 13:57:33 +0300

    using Palidrome

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+  const cleanStr = str.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    const reversedStr = cleanStr.split('').reverse().join('');
+    return cleanStr === reversedStr;
 }
 
-/* 
-  Add your pseudocode here
-*/
+function displayResult() {
+  const inputText = document.getElementById('inputText').value;
+  const result = isPalindrome(inputText);
+  const resultMessage = result ? 'It is a palindrome!' : 'It is not a palindrome.';
 
-/*
-  Add written explanation of your solution here
-*/
+  document.getElementById('result').innerText = resultMessage;
+}
+
+// Additional Test Cases
+console.log(isPalindrome("radar")); // It is a palindrome! (Console output)
+console.log(isPalindrome("Hello")); // It is not a palindrome. (Console output)
+console.log(isPalindrome("A man, a plan, a canal, Panama!")); // It is a palindrome! (Console output)
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
